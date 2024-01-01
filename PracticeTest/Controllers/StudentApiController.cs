d27a5da344b00d70004201be40f6332a2d3221a0
kaviya <Admin@KAVYANITHI>
2024-01-01 18:32:20 +0530

    Add project files.

---
+++ PracticeTest/Controllers/StudentApiController.cs
@@ -0,0 +1,46 @@
+﻿
+using PracticeTest.DAO;
+using PracticeTest.Models;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace AttendenceTracker.Controllers
+{
+    public class StudentApiController : ApiController
+    {
+        private readonly AttendanceRepository attendanceRepository = new
+AttendanceRepository();
+
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return attendanceRepository.GetStudentData();
+        }
+
+        public IHttpActionResult GetStudent(int id)
+        {
+            var student = attendanceRepository.GetStudentById(id);
+            return student != null ? (IHttpActionResult)Ok(student) : NotFound();
+        }
+
+        public IHttpActionResult PostStudent(Student student)
+        {
+            attendanceRepository.AddStudent(student);
+            return CreatedAtRoute("DefaultApi", new { id = student.Id }, student);
+        }
+
+
+        public IHttpActionResult PutStudent(int id, Student student)
+        {
+            attendanceRepository.EditStudent(student);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        public IHttpActionResult DeleteStudent(int id)
+        {
+            attendanceRepository.DeleteStudent(id);
+            return Ok();
+        }
+    }
+}
---
+++ PracticeTest/DAO/AttendanceRepository.cs
@@ -0,0 +1,47 @@
+﻿using PracticeTest.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PracticeTest.DAO
+{
+    public class AttendanceRepository
+    {
+        private masterEntities db = new masterEntities();
+
+        public IEnumerable<Student> GetStudentData()
+        {
+            return db.Students.ToList();
+        }
+
+        public Student GetStudentById(int id)
+        {
+            return db.Students.Find(id);
+        }
+
+        public void AddStudent(Student student)
+        {
+            db.Students.Add(student);
+            db.SaveChanges();
+        }
+
+        public void EditStudent(Student student)
+        {
+            if (student != null)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public void DeleteStudent(int id)
+        {
+            var student = db.Students.Find(id);
+            if (student != null)
+            {
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+        }
+    }
+}
